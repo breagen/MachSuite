53fefd7232113fb01591799c0d1a29529dac3e9f
Bob Adolf <rdadolf@gmail.com>
2015-06-25 15:01:23 -0400

    Fix gcc induction variable definition bug.

--- bfs/bulk/support.c
+++ bfs/bulk/support.c
@@ -22,11 +22,12 @@ void input_to_data(int fd, void *vdata) {
   struct bench_args_t *data = (struct bench_args_t *)vdata;
   char *p, *s;
   uint64_t *nodes;
+  int64_t i;
 
   // Zero-out everything.
   memset(vdata,0,sizeof(struct bench_args_t));
   // Max-ify levels
-  for(int64_t i=0; i<N_NODES; i++) {
+  for(i=0; i<N_NODES; i++) {
     data->level[i]=MAX_LEVEL;
   }
   // Load input string
@@ -39,7 +40,7 @@ void input_to_data(int fd, void *vdata) {
   s = find_section_start(p,2);
   nodes = (uint64_t *)malloc(N_NODES*2*sizeof(uint64_t));
   parse_uint64_t_array(s, nodes, N_NODES*2);
-  for(int64_t i=0; i<N_NODES; i++) {
+  for(i=0; i<N_NODES; i++) {
     data->nodes[i].edge_begin = nodes[2*i];
     data->nodes[i].edge_end = nodes[2*i+1];
   }
@@ -51,6 +52,7 @@ void input_to_data(int fd, void *vdata) {
 
 void data_to_input(int fd, void *vdata) {
   uint64_t *nodes;
+  int64_t i;
 
   struct bench_args_t *data = (struct bench_args_t *)vdata;
   // Section 1: starting node
@@ -59,7 +61,7 @@ void data_to_input(int fd, void *vdata) {
   // Section 2: node structures
   write_section_header(fd);
   nodes = (uint64_t *)malloc(N_NODES*2*sizeof(uint64_t));
-  for(int64_t i=0; i<N_NODES; i++) {
+  for(i=0; i<N_NODES; i++) {
     nodes[2*i]  = data->nodes[i].edge_begin;
     nodes[2*i+1]= data->nodes[i].edge_end;
   }
@@ -98,9 +100,10 @@ int check_data( void *vdata, void *vref ) {
   struct bench_args_t *data = (struct bench_args_t *)vdata;
   struct bench_args_t *ref = (struct bench_args_t *)vref;
   int has_errors = 0;
+  int i;
 
   // Check that the horizons have the same number of nodes
-  for(int i=0; i<N_LEVELS; i++) {
+  for(i=0; i<N_LEVELS; i++) {
     has_errors |= (data->level_counts[i]!=ref->level_counts[i]);
   }
 
