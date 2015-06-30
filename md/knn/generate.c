d41eb2fc7d2f06bd2caf1b516792c2958653e726
Bob Adolf <rdadolf@gmail.com>
2015-06-30 13:01:40 -0400

    Minor fix for gcc warnings.

--- md/knn/generate.c
+++ md/knn/generate.c
@@ -88,4 +88,6 @@ int main(int argc, char **argv)
   fd = open("input.data", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
   assert( fd>0 && "Couldn't open input data file" );
   data_to_input(fd, (void *)(&data));
+
+  return 0;
 }
